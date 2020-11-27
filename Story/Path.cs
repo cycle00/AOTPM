a555913357a68b02628130e7e1fa0b6fc3113848
Dmitry Vakulenko <dmitry.a.vakulenko@gmail.com>
2020-11-27 12:42:34 -0500

    path creation ig huh what

--- Story/Path.cs
+++ Story/Path.cs
@@ -1,7 +1,11 @@
 using System;
 
 namespace AOTPM.Story {
-    public interface Path {
+    public abstract class Path {
+        public abstract int NumOfSubPaths { get; set; }
         
+        public virtual void Event() {
+            Console.WriteLine("Please add an event or more.");
+        }
     }
 }
